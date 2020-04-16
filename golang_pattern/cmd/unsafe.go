7b49af2996dd137f5a0175152ff9bdc0685a2314
lunzhoufei <lunzhoufei@gmail.com>
2020-04-17 01:33:08 +0800

    add golang pkg util

--- golang_pattern/cmd/unsafe.go
+++ golang_pattern/cmd/unsafe.go
@@ -19,12 +19,11 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-	"sync"
 )
 
-// onceCmd represents the once command
-var onceCmd = &cobra.Command{
-	Use:   "once",
+// unsafeCmd represents the unsafe command
+var unsafeCmd = &cobra.Command{
+	Use:   "unsafe",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -33,42 +32,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("once called")
-		doTestOnce()
+		fmt.Println("unsafe called")
 	},
 }
 
 func init() {
-	concurrencyCmd.AddCommand(onceCmd)
+	rootCmd.AddCommand(unsafeCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// onceCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// unsafeCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// onceCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func doTestOnce() {
-	var once sync.Once
-
-	onceBody := func() {
-		// do some stuff
-		fmt.Println("this will Only execute once")
-	}
-
-	done := make(chan bool)
-	for i := 0; i < 10; i++ {
-		go func() {
-			once.Do(onceBody)
-			done <- true
-		}()
-	}
-
-	for i := 0; i < 10; i++ {
-		<-done
-	}
+	// unsafeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
